370e43381f7c450e6cb63518baa2dcc351acc3e4
Courtney Meyerhofer <meyerhoferc@gmail.com>
2016-12-27 14:20:10 -0700

    finishes hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,16 @@
-require 'pry'
-class Hamming
+module BookKeeping
+  VERSION = 3
+end
 
+class Hamming
   def self.compute(string_1, string_2)
+    string_1.length == string_2.length ? true : raise(ArgumentError)
     @counter = 0
     string_1 = string_1.chars
     string_2 = string_2.chars
-    string_1.each do |letter|
-      letter_index = string_1.index(letter)
-      letter == string_2[letter_index] ? @counter : @counter += 1
+    string_1.each_with_index do |letter, index|
+      letter == string_2[index] ? @counter : @counter += 1
     end
     @counter
   end
-
 end
